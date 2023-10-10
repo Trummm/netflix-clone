81054e65c8e9b463bddad7da1b469c382a960863
Trummm <trungpro2000.tp2@gmail.com>
2023-10-10 11:12:09 +0700

    update

---
+++ pages/auth.tsx
@@ -0,0 +1,7 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover"></div>
+  )
+}
+
+export default Auth

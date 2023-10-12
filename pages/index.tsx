0ed05b50ea689a09c9a224d8dbe082cd766ef69e
Trummm <trungpro2000.tp2@gmail.com>
2023-10-12 11:19:55 +0700

    update last

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ import useCurrentUser from '@/hooks/useCurrentUser'
 import Navbar from '@/components/Navbar'
 import Billboard from '@/components/Billboard'
 import MovieList from '@/components/MovieList'
-// import InfoModal from '@/components/InfoModal'
+import InfoModal from '@/components/InfoModal'
 import useMovieList from '@/hooks/useMovieList'
 import useFavorites from '@/hooks/useFavorites'
-// import useInfoModalStore from '@/hooks/useInfoModalStore'
+import useInfoModalStore from '@/hooks/useInfoModalStore'
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -32,11 +32,11 @@ const Home = () => {
   const { data: user } = useCurrentUser()
   const { data: movies = [] } = useMovieList()
   const { data: favorites = [] } = useFavorites()
-  // const { isOpen, closeModal } = useInfoModalStore()
+  const { isOpen, closeModal } = useInfoModalStore()
 
   return (
     <>
-      {/* <InfoModal visible={isOpen} onClose={closeModal} /> */}
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className='pb-40'>
